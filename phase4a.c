bd274ecd44ad5a7fbbbc8137c9aed76e58177165
Ishika Patel <106274541+ishikapatel2@users.noreply.github.com>
2024-03-31 13:04:15 -0700

    initial commit pt.2

--- phase4a.c
+++ phase4a.c
@@ -4,26 +4,30 @@
 #include <string.h>
 #include <stdlib.h>
 
-int  Sleep(int seconds){
+void phase4_init(void) {
+    return;
+}
+
+int  kernSleep(int seconds) {
     return 0;
 }
 
-int TermRead(char *buffer, int bufSize, int unit, int *lenOut) {
+int  kernDiskRead (void *diskBuffer, int unit, int track, int first, int sectors, int *status) {
     return 0;
 }
 
-int TermWrite(char *buffer, int bufSize, int unit, int *lenOut) {
+int  kernDiskWrite(void *diskBuffer, int unit, int track, int first, int sectors, int *status) {
     return 0;
 }
 
-int DiskSize(int unit, int *sector, int *track, int *disk) {
+int  kernDiskSize (int unit, int *sector, int *track, int *disk) {
     return 0;
 }
 
-int DiskRead(void *buffer, int unit, int track, int firstBlock, int blocks, int *statusOut) {
+int  kernTermRead (char *buffer, int bufferSize, int unitID, int *numCharsRead) {
     return 0;
 }
 
-int DiskWrite(void *diskBuffer, int unit, int track, int first, int sectors, int *status) {
+int  kernTermWrite(char *buffer, int bufferSize, int unitID, int *numCharsRead) {
     return 0;
 }
