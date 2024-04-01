cf0a4ce6d45547664ec071d8ef407e47ef72dd60
Hamad <marhoon@email.arizona.edu>
2024-04-01 13:30:34 -0700

    make working

--- phase4a.c
+++ phase4a.c
@@ -1,41 +1,52 @@
+
 #include <stdio.h>
-#include "phase4.h"
-#include "phase4_usermode.h"
+#include <phase1.h>
+#include <phase2.h>
+#include <phase3.h>
+#include <phase4.h>
 #include <string.h>
 #include <stdlib.h>
+#include <stdint.h>
 
-void phase4_init(void) {
+void phase4_init(void)
+{
     return;
 }
 
-void phase4_start_service_processes(void) {
+void phase4_start_service_processes(void)
+{
     return;
 }
 
-int  kernSleep(int seconds) {
+int kernSleep(int seconds)
+{
     return 0;
 }
 
-int  kernTermRead (char *buffer, int bufferSize, int unitID, int *numCharsRead) {
+int kernTermRead(char *buffer, int bufferSize, int unitID, int *numCharsRead)
+{
     return 0;
 }
 
-int  kernTermWrite(char *buffer, int bufferSize, int unitID, int *numCharsRead) {
-    return 0;
-}
-
-// 4b
-int  kernDiskRead (void *diskBuffer, int unit, int track, int first, int sectors, int *status) {
-    return 0;
-}
-
-// 4b
-int  kernDiskWrite(void *diskBuffer, int unit, int track, int first, int sectors, int *status) {
+int kernTermWrite(char *buffer, int bufferSize, int unitID, int *numCharsRead)
+{
     return 0;
 }
 
 // 4b
-int  kernDiskSize (int unit, int *sector, int *track, int *disk) {
+int kernDiskRead(void *diskBuffer, int unit, int track, int first, int sectors, int *status)
+{
     return 0;
 }
 
+// 4b
+int kernDiskWrite(void *diskBuffer, int unit, int track, int first, int sectors, int *status)
+{
+    return 0;
+}
+
+// 4b
+int kernDiskSize(int unit, int *sector, int *track, int *disk)
+{
+    return 0;
+}
