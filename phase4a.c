db327cdc6137a3e60c78c54d00742570b536e8ac
Ishika Patel <106274541+ishikapatel2@users.noreply.github.com>
2024-04-01 10:36:22 -0700

    testcases passing but no code written

--- phase4a.c
+++ phase4a.c
@@ -8,19 +8,11 @@ void phase4_init(void) {
     return;
 }
 
+void phase4_start_service_processes(void) {
+    return;
+}
+
 int  kernSleep(int seconds) {
-    return 0;
-}
-
-int  kernDiskRead (void *diskBuffer, int unit, int track, int first, int sectors, int *status) {
-    return 0;
-}
-
-int  kernDiskWrite(void *diskBuffer, int unit, int track, int first, int sectors, int *status) {
-    return 0;
-}
-
-int  kernDiskSize (int unit, int *sector, int *track, int *disk) {
     return 0;
 }
 
@@ -31,3 +23,19 @@ int  kernTermRead (char *buffer, int bufferSize, int unitID, int *numCharsRead) 
 int  kernTermWrite(char *buffer, int bufferSize, int unitID, int *numCharsRead) {
     return 0;
 }
+
+// 4b
+int  kernDiskRead (void *diskBuffer, int unit, int track, int first, int sectors, int *status) {
+    return 0;
+}
+
+// 4b
+int  kernDiskWrite(void *diskBuffer, int unit, int track, int first, int sectors, int *status) {
+    return 0;
+}
+
+// 4b
+int  kernDiskSize (int unit, int *sector, int *track, int *disk) {
+    return 0;
+}
+
